9a821a870dcf973830b2489f9765891be7c3727f
Jason Peters <jpeters1228@comcast.net>
2019-03-07 23:21:39 -0700

    Logic part 2

--- app/src/main/java/com/shelatok/bardice/GameActivity.kt
+++ app/src/main/java/com/shelatok/bardice/GameActivity.kt
@@ -1,13 +1,19 @@
 package com.shelatok.bardice
 
 import android.content.Intent
+import android.content.IntentSender
+import android.media.Image
 import android.os.*
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.View.INVISIBLE
 import kotlinx.android.synthetic.main.activity_game.*
 import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import android.widget.ToggleButton
+import com.shelatok.bardice.R.drawable.d6_1
 import java.util.*
 import kotlin.math.absoluteValue
 import kotlin.system.exitProcess
@@ -16,107 +22,112 @@ class GameActivity : AppCompatActivity() {
 
     var roll = 0
     var highScore = 0
+    var currentScore = 0
     var numdice = 0
+    var numones = 0
     var highdie = 0
+    var allnums = arrayListOf(0,0,0,0,0,0)
     var allDice = arrayListOf(0,0,0,0,0)
+    var totalrolls = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
     }
 
     fun rollDice() : Int {
         return Random().nextInt(6) + 1
     }
 
+    fun score() {
+        val keepButtonArray = arrayListOf<ToggleButton>(keepButton1, keepButton2, keepButton3, keepButton4, keepButton5)
+        for (a in 1..keepButtonArray.count()){
+            keepButtonArray[a-1].visibility = INVISIBLE
+        }
+        playAgainButton.visibility = VISIBLE
+        newScoreButton.visibility = VISIBLE
+        endButton.visibility = VISIBLE
+        rollButton.visibility = INVISIBLE
+        keepScoreButton.visibility = INVISIBLE
+        for (a in 1..allDice.count()){
+            if (allDice[a-1] == 1){
+                numones++
+            }
+        }
+        for (a in 1..allDice.count()){
+            allnums[allDice[a-1]-1]++
+        }
+        for (a in 2..allnums.count()){
+            if (a >= highdie && allnums[a-1] >= numdice){
+                highdie = a
+                numdice = allnums[a-1]
+            }
+        }
+        numdice = numones + numdice
+        currentScore = (numdice * 10) + highdie
+        if (!allDice.contains(1)){
+            currentScore = 0
+        }
+        currentScoreText.text = "Your Score: $currentScore in $roll"
+        println(allDice)
+        if (currentScore > highScore){
+            if (highScore != 0) {
+                Toast.makeText(this, "You Win!", Toast.LENGTH_SHORT).show()
+            }
+            highScore = currentScore
+            scoreText.text = "High Score: $highScore in $roll"
+        }
+    }
+
 
 
     fun onRollClicked(view: View){
 
-        keepButton1.visibility = VISIBLE
-        keepButton2.visibility = VISIBLE
-        keepButton3.visibility = VISIBLE
-        keepButton4.visibility = VISIBLE
-        keepButton5.visibility = VISIBLE
-        keepScoreButton.visibility = VISIBLE
+        val keepButtonArray = arrayListOf<ToggleButton>(keepButton1, keepButton2, keepButton3, keepButton4, keepButton5)
+        val imgVArray = arrayListOf<ImageView>(die1, die2, die3, die4, die5)
+        val dicePicArray = arrayListOf<Int>(R.drawable.d6_1, R.drawable.d6_2, R.drawable.d6_3, R.drawable.d6_4,
+            R.drawable.d6_5, R.drawable.d6_6)
 
-        if (roll < 3) {
-            if (!keepButton1.isChecked) {
-                var rollnum = rollDice()
-                when (rollnum) {
-                    1 -> die1.setImageResource(R.drawable.d6_1)
-                    2 -> die1.setImageResource(R.drawable.d6_2)
-                    3 -> die1.setImageResource(R.drawable.d6_3)
-                    4 -> die1.setImageResource(R.drawable.d6_4)
-                    5 -> die1.setImageResource(R.drawable.d6_5)
-                    6 -> die1.setImageResource(R.drawable.d6_6)
-                }
-                allDice[0] = rollnum
-            }
+        for (a in 1..keepButtonArray.count()){
+            keepButtonArray[a-1].visibility = VISIBLE
+        }
+        if (highScore == 0) {
+            keepScoreButton.visibility = VISIBLE
+        }
 
-            if (!keepButton2.isChecked) {
-                var rollnum = rollDice()
-                when (rollnum) {
-                    1 -> die2.setImageResource(R.drawable.d6_1)
-                    2 -> die2.setImageResource(R.drawable.d6_2)
-                    3 -> die2.setImageResource(R.drawable.d6_3)
-                    4 -> die2.setImageResource(R.drawable.d6_4)
-                    5 -> die2.setImageResource(R.drawable.d6_5)
-                    6 -> die2.setImageResource(R.drawable.d6_6)
-                }
-                allDice[1] = rollnum
-            }
+        if (roll < totalrolls) {
 
-            if (!keepButton3.isChecked) {
-                var rollnum = rollDice()
-                when (rollnum) {
-                    1 -> die3.setImageResource(R.drawable.d6_1)
-                    2 -> die3.setImageResource(R.drawable.d6_2)
-                    3 -> die3.setImageResource(R.drawable.d6_3)
-                    4 -> die3.setImageResource(R.drawable.d6_4)
-                    5 -> die3.setImageResource(R.drawable.d6_5)
-                    6 -> die3.setImageResource(R.drawable.d6_6)
+            val countDownTimer = object: CountDownTimer(1000, 100) {
+                override fun onTick(millisUntilFinished: Long) {
+                    for (a in 1..keepButtonArray.count()) {
+                        if (!keepButtonArray[a-1].isChecked){
+                            var rollnum = rollDice()
+                            imgVArray[a-1].setImageResource(dicePicArray[rollnum-1])
+                            allDice[a-1] = rollnum
+                        }
+                    }
                 }
-                allDice[2] = rollnum
-            }
+                override fun onFinish() {
 
-            if (!keepButton4.isChecked) {
-                var rollnum = rollDice()
-                when (rollnum) {
-                    1 -> die4.setImageResource(R.drawable.d6_1)
-                    2 -> die4.setImageResource(R.drawable.d6_2)
-                    3 -> die4.setImageResource(R.drawable.d6_3)
-                    4 -> die4.setImageResource(R.drawable.d6_4)
-                    5 -> die4.setImageResource(R.drawable.d6_5)
-                    6 -> die4.setImageResource(R.drawable.d6_6)
                 }
-                allDice[3] = rollnum
             }
+            countDownTimer.start()
 
-            if (!keepButton5.isChecked) {
-                var rollnum = rollDice()
-                when (rollnum) {
-                    1 -> die5.setImageResource(R.drawable.d6_1)
-                    2 -> die5.setImageResource(R.drawable.d6_2)
-                    3 -> die5.setImageResource(R.drawable.d6_3)
-                    4 -> die5.setImageResource(R.drawable.d6_4)
-                    5 -> die5.setImageResource(R.drawable.d6_5)
-                    6 -> die5.setImageResource(R.drawable.d6_6)
-                }
-                allDice[4] = rollnum
-            }
             roll++
-            rollText.text = "Current Roll: $roll"
-            if (roll == 3) {
-                playAgainButton.visibility = VISIBLE
-                newScoreButton.visibility = VISIBLE
-                endButton.visibility = VISIBLE
-                rollButton.visibility = INVISIBLE
-                keepScoreButton.visibility = INVISIBLE
+            rollText.text = "Roll: $roll"
 
-                for (a in 1..allDice.count()){
-
+            if (roll == totalrolls) {
+                val counttimer = object: CountDownTimer(1000, 100) {
+                    override fun onTick (millisFinished: Long){
+                    }
+                    override fun onFinish () {
+                        score()
+                    }
                 }
+                counttimer.start()
+
+
             }
         }
 
@@ -125,40 +136,52 @@ class GameActivity : AppCompatActivity() {
     }
 
     fun onKeepClicked(view: View){
+        val keepButtonArray = arrayListOf<ToggleButton>(keepButton1, keepButton2, keepButton3, keepButton4, keepButton5)
         if (!allDice.contains(1)){
             Toast.makeText(this, "You need a 1 to keep.", Toast.LENGTH_SHORT).show()
-            keepButton1.isChecked = false
-            keepButton2.isChecked = false
-            keepButton3.isChecked = false
-            keepButton4.isChecked = false
-            keepButton5.isChecked = false
+            for (a in 1..keepButtonArray.count()){
+                keepButtonArray[a-1].isChecked = false
+            }
         }
     }
 
+    fun onKeepScoreClick(view: View){
+        totalrolls = roll
+        score()
+    }
+
     fun onPlayAgainClick (view: View){
+        val keepButtonArray = arrayListOf<ToggleButton>(keepButton1, keepButton2, keepButton3, keepButton4, keepButton5)
+        numones = 0
         roll = 0
+        highdie = 0
+        numdice = 0
+        for (a in 1..6){
+            allnums[a-1] = 0
+        }
+        for (a in 1..allDice.count()){
+            allDice[a-1] = 0
+        }
+        currentScoreText.text = "Your Score: Not Set"
         playAgainButton.visibility = INVISIBLE
         newScoreButton.visibility = INVISIBLE
         endButton.visibility = INVISIBLE
         rollButton.visibility = VISIBLE
-        keepButton1.isChecked = false
-        keepButton2.isChecked = false
-        keepButton3.isChecked = false
-        keepButton4.isChecked = false
-        keepButton5.isChecked = false
-        keepButton1.visibility = INVISIBLE
-        keepButton2.visibility = INVISIBLE
-        keepButton3.visibility = INVISIBLE
-        keepButton4.visibility = INVISIBLE
-        keepButton5.visibility = INVISIBLE
-        rollText.text = "Current Roll: $roll"
+
+        for (a in 1..keepButtonArray.count()){
+            keepButtonArray[a-1].isChecked = false
+            keepButtonArray[a-1].visibility = INVISIBLE
+        }
+        rollText.text = "Roll: $roll"
     }
 
     fun onNewScoreClick (view: View){
         onPlayAgainClick(view)
-        var highScore = 0
+        totalrolls = 3
+        highScore = 0
         var numdice = 0
         var highdie = 0
+        scoreText.text = "High Score: Not Set"
     }
 
     fun onEndClick(view: View){
